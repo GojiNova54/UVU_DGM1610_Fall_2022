42b5903a8ae5385908bf32d67f2436f5381a88bc
GojiNova54 <111925821+GojiNova54@users.noreply.github.com>
2022-08-31 15:57:11 -0600

    More Practice

--- Unit 1/Unit 1a/Unit1a_Lab.cs
+++ Unit 1/Unit 1a/Unit1a_Lab.cs
@@ -12,6 +12,11 @@ namespace MyApplication
       Console.WriteLine(text1);
       int year = 3025;
       Console.WriteLine(year);
+      string text2 = "The Clans are planning to invade in:";
+      Console.WriteLine(text2);
+      int year2 = 3025;
+      year2 += 25;
+      Console.WriteLine(year2);
      }
   }
 }
