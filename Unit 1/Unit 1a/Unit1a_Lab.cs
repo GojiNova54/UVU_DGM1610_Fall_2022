cd88473d048ab629a9d61138de694d0947fed9f1
GojiNova54 <111925821+GojiNova54@users.noreply.github.com>
2022-08-31 15:45:21 -0600

    Practice

---
+++ Unit 1/Unit 1a/Unit1a_Lab.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Commander Sean";
+      Console.WriteLine(name); 
+      string text1 = "The year is:";
+      Console.WriteLine(text1);
+      int year = 3025;
+      Console.WriteLine(year);
+     }
+  }
+}
