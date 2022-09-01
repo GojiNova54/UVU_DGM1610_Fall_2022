1ab415cc0c92300d8ca60805662004de24bf51ab
GojiNova54 <111925821+GojiNova54@users.noreply.github.com>
2022-09-01 10:39:22 -0600

    year change

--- Unit 1/Unit 1a/Unit1a_Lab.cs
+++ Unit 1/Unit 1a/Unit1a_Lab.cs
@@ -15,7 +15,7 @@ namespace MyApplication
       string text2 = "The Clans are planning to invade in:";
       Console.WriteLine(text2);
       int year2 = 3025;
-      year2 += 25;
+      year2 += 24;
       Console.WriteLine(year2);
      }
   }
