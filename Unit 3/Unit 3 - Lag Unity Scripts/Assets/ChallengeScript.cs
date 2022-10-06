5008caa4e39862c21af121a78845b49ee00c4c5d
Sean Anderson <nova7prospekt@gmail.com>
2022-10-06 10:49:06 -0600

    Added float to Challenge Script

--- Unit 3/Unit 3 - Lag Unity Scripts/Assets/ChallengeScript.cs
+++ Unit 3/Unit 3 - Lag Unity Scripts/Assets/ChallengeScript.cs
@@ -19,4 +19,34 @@ public class ChallengeScript : MonoBehaviour
         result = number / 2;
         return result;
     }
+
+    float subDepth = 800;
+    float decreasedDepthLimit = 600;
+    float nominalDepthLimit = 100;
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            DepthTest();
+
+        subDepth -= Time.deltaTime * 40;
+    }
+
+    void DepthTest () 
+    {
+        if (subDepth > decreasedDepthLimit)
+        {
+            print("Warning, sub at critical depth limit.");
+        }
+
+        else if (subDepth < nominalDepthLimit)
+        {
+            print("Warning, approaching unbreachable surface.");
+        }
+
+        else 
+        {
+            print("Submarine is at nominal depth.");
+        }
+    }
 }
