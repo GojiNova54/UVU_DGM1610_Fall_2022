a0b2fdd339f13f6c95b0f104f623ae8e0294f07c
Sean Anderson <nova7prospekt@gmail.com>
2022-10-18 14:24:06 -0600

    Added Unit 5 Lab - Unity Events

---
+++ Unit 5/Unity Events/Assets/MonoEventsBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent, awakeEvent, disableEvent;
+
+    public void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    public void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void OnDisable()
+    {
+        disableEvent.Invoke();
+    }
+}
