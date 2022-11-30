944285a154da15e9e14d6f1854bda1019ea4ef2d
Sean Anderson <nova7prospekt@gmail.com>
2022-11-30 16:26:33 -0700

    Added Uni7c Challenge
    Additional sounds, animations, and particle system

--- Unit_7/Matching Game/Assets/Lab7c/RestartGame2.cs
+++ Unit_7/Matching Game/Assets/Lab7c/RestartGame2.cs
@@ -7,6 +7,12 @@ public class RestartGame2 : MonoBehaviour
 {
     public void LoadGame() 
     {
+        StartCoroutine(DelaySceneLoad());
+    }
+
+    IEnumerator DelaySceneLoad() 
+    {
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene("GameScene");
     }
 }
